91f7e3bfc840ef74867ff959e6d137040680ee47
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-08-24 22:18:37 -0700

    code clean up

--- javascript/controllers/mainController.js
+++ javascript/controllers/mainController.js
@@ -1,10 +1,5 @@
 app.controller('mainController', ['$scope', 'forecast', '$http', function($scope, forecast, $http){
 	forecast.success(function(data){
-		// $scope.location = data.current_observation.display_location.full;
-		// $scope.temperature = data.current_observation.temp_f;
-		// $scope.weather = data.current_observation.weather;
-		// $scope.icon = data.current_observation.icon_url;
-
 		$scope.forecast = {
 			location: data.current_observation.display_location.full,
 			zip: data.current_observation.display_location.zip,
--- javascript/directives/forecastData.js
+++
@@ -1,9 +0,0 @@
-app.directive('forecastData', function(){
-  return {
-    restrict: 'E',
-    scope: {
-      info: '='
-    },
-    templateUrl: 'javascript/directives/forecastData.html'
-  };
-});
